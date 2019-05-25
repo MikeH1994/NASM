806b5d8bf6664458ed525e59a62d3299a7f387da
MikeH1994 <Michael.Hayes.2013@live.rhul.ac.uk>
2019-05-25 16:16:00 +0100

    added 004

--- 002_helloWorld/hello.asm
+++ 002_helloWorld/hello.asm
@@ -4,19 +4,19 @@
 ; Run with: ./helloworld
  
 SECTION .data           ;constant data goes here
-msg     db      'Herro World!', 0Ah     ; (0Ah here is line feed character)
+msg     db      'Eyyy World!', 0Ah     ; (0Ah here is line feed character)
  
 SECTION .text           ; code goes here   
 global  _start          ; defining default entry point for program
 
 _start:
     mov     rax, 1      ; opcode
-    mov     rdi, 1      ; output file
-    mov     rsi, msg    ; string
-    mov     rdx, 13     ; length of string (12 + 1 for line feed character)
+    mov     rdi, 2      ; output file fd
+    mov     rsi, msg    ; char buffer to be written to fd
+    mov     rdx, 12     ; length of msg
     syscall
  
-    mov     rax, 60      ; invoke SYS_EXIT (kernel opcode 1)
+    mov     rax, 60     ; invoke SYS_EXIT (kernel opcode 1)
     mov     rdi, 0      ; return 0 status on exit - 'No Errors'
     syscall
 
---
+++ 004_stringLen_subroutines/helloworld-len.asm
@@ -0,0 +1,40 @@
+; Hello World Program (Subroutines)
+; Compile with: nasm -f elf helloworld-len.asm
+; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld-len.o -o helloworld-len
+; Run with: ./helloworld-len
+ 
+SECTION .data
+msg     db      'Hello, brave new world!', 0Ah
+ 
+SECTION .text
+global  _start
+ 
+_start:
+ 
+    mov     rax, msg        ; move the address of msg in to rax
+    call    strlen          ; call calculate length of msg by calling subroutine strlen
+
+    mov     rax, 1          ; opcode for sys_write
+    mov     rdi, 1          ; file descriptor
+    mov     rsi, msg        ; char* buffer
+    mov     rdx, rax        ; strlen 
+    syscall
+ 
+    mov     rax, 60         ; opcode for sys_exit
+    mov     rdi, 0          ; error code on sys_exit
+    syscall
+
+strlen:                     ; this is our first function declaration
+    push    rdi             ; push the value in rdi onto the stack to preserve it while we use rdi in this function
+    mov     rdi, rax        ; move the address in rax into rdi (Both point to the same segment in memory)
+ 
+nextchar:                   
+    cmp     byte [rax], 0   ;if the byte at address held in rax is zero, set zero flag to 1
+    jz      finished        ;if zero flag is 1, jump to finished
+    inc     rax             ;else, increment rax
+    jmp     nextchar        ;
+ 
+finished:
+    sub     rax, rdi        ; rdi = start of buffer, rax = end of buffer; rax-rdi=length of string
+    pop     rdi             ; pop the value on the stack back into rdi
+    ret                     ; return to where the function was called
